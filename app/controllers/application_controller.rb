21025be8755e5eb2a8c310b2eb880aeeed0fc637
djo <joseph@tilkee.com>
2016-11-20 10:10:13 +0100

    add route /me

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::API
+
+  before_action :authenticate!
+
+
+  def me
+    render json: @current_user.to_json
+  end
+
+
+private
+
+  def authenticate!
+    @current_user = User.find_by_id(request.env["HTTP_USER_ID"])
+    head 401 if @current_user.nil?
+  end
+
+  def current_user
+    @current_user
+  end
+
+
 end
